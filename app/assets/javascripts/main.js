1fdb5904cc5e578357568c742b2f36c9ead96a86
deveshrizala-c-metric <deveshrizala-c-metric@c-metric.com>
2017-01-30 11:34:47 +0530

    B009-Disable-Button

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,3 +3,17 @@ $(function(){
     $('.alert').remove();
   }, 5000);
  });
+
+document.getElementById("reset_password").onclick = function() {
+    //disable
+    this.disabled = true;
+
+    //do some validation stuff
+}
+
+document.getElementById("resend_confirmation").onclick = function() {
+    //disable
+    this.disabled = true;
+
+    //do some validation stuff
+}
