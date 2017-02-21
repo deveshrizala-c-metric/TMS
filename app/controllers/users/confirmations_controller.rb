9e31af7fc4214fd3cab5095cbfe56ab978571c9b
deveshrizala-c-metric <deveshrizala-c-metric@c-metric.com>
2017-02-21 13:02:03 +0530

    S010-Check-Email-Format

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -5,9 +5,19 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # end
 
   # POST /resource/confirmation
-  # def create
-  #   super
-  # end
+  def create
+    if params[:user][:email].blank?
+      flash[:danger] = "Email can't be blank."
+      redirect_to new_user_confirmation_path
+    else
+      if validate_email(params[:user][:email])
+        super
+      else
+        flash[:danger] = 'Invalid Email format.'
+        redirect_to new_user_confirmation_path
+      end
+    end
+  end
 
   # GET /resource/confirmation?confirmation_token=abcdef
   # def show
@@ -25,4 +35,8 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # def after_confirmation_path_for(resource_name, resource)
   #   super(resource_name, resource)
   # end
+
+  def validate_email(email)
+    (email =~ /^(([A-Za-z0-9])\w+(\.?|\-?|\+))+@{1}(([A-Za-z])\w*-?([A-Za-z])\w*?\.{1}){1,2}([A-Za-z])\w+$/) ? true : false
+  end
 end
