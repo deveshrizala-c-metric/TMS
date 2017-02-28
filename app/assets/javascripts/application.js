59b198192d9b4d391e6c3f573b2394c02a3489f2
deveshrizala-c-metric <deveshrizala-c-metric@c-metric.com>
2017-02-28 12:13:57 +0530

    #009-Bootstrap CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require jquery_ujs
 //= require jquery.dataTables.js
 //= require dataTables.bootstrap.js
+//= require fileinput
 //= require tinymce
 //= require turbolinks
 //= require_tree .
